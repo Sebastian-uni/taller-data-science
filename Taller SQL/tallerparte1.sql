c09ad76224ea888ee33b4953552f6ed46b3ad441
Gabriela_Rojas <gabrielarojas222@unisangil.edu.co>
2026-06-29 19:54:14 -0500

    Agrego cambios del taller SQL

---
+++ Taller SQL/tallerparte1.sql
@@ -0,0 +1,21 @@
+CREATE DATABASE ejercicios;
+
+USE ejercicios;
+
+CREATE TABLE fuel_consumption (
+    MODELYEAR INT,                         -- Año de fabricación (ej. 2014)
+    MAKE VARCHAR(50),                      -- Fabricante / Marca (ej. ACURA)
+    MODEL VARCHAR(100),                    -- Modelo del vehículo (ej. ILX)
+    VEHICLECLASS VARCHAR(50),              -- Clase de vehículo (ej. COMPACT)
+    ENGINESIZE DECIMAL(3,1),               -- Tamaño del motor en litros (ej. 2.0)
+    CYLINDERS INT,                         -- Número de cilindros (ej. 4)
+    TRANSMISSION VARCHAR(10),              -- Tipo de transmisión (ej. AS5, M6)
+    FUELTYPE VARCHAR(5),                   -- Tipo de combustible (ej. Z, D, X)
+    FUELCONSUMPTION_CITY DECIMAL(4,1),     -- Consumo en ciudad (L/100 km)
+    FUELCONSUMPTION_HWY DECIMAL(4,1),      -- Consumo en carretera (L/100 km)
+    FUELCONSUMPTION_COMB DECIMAL(4,1),     -- Consumo combinado (L/100 km)
+    FUELCONSUMPTION_COMB_MPG INT,          -- Consumo combinado en Millas por Galón (mpg)
+    CO2EMISSIONS INT                       -- Emisiones de CO2 (g/km)
+);
+
+-- Paso 1.3 Importación del dataset en MySQL Workbench: Se usó dando clic derecho la opción de Table Data Import Wizard.
